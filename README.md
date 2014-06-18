f04918a30855b59d4c62962340fc00b4299f7cc8
KspR <KspR@kspr.local>
2014-06-18 14:42:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+## Seedjs : fast, light and simple JavaScript Class and Evented Object
+------------------------------------------------------
+Seedjs is a simple inheritable and event-driven Class that allows you to do elegant OOP.
+If you include [toDOM](https://github.com/KspR/toDOM) too you can bind DOM elements to Seed.
+
+### When to use?
+Event-driven architecture will help reduce the coupling between different modules of your application. Inheritance will help for maintenance and readability of the code.
+
+### Installing Seedjs
+Just include Seed.js somewhere in your < body > and you are ready to go!
+
+If you want to use Seed's View capability, you will have to include [toDOM](https://github.com/KspR/toDOM) too.
+
+```html
+<script src="http://raw.githubusercontent.com/KspR/Seedjs/master/Seed.js"></script>
+```
+
+### Usage
+#### Basics
+```javascript
+var MyClass = r.Seed.extend({
+  
+  init : function(options) { // called at instanciation
+    console.log('init');
+  }
+
+});
+
+new MyClass({
+  some : 'options'
+}); // will log 'init'
+```
+
+#### Inheritance
+```javascript
+var MySubClass = MyClass.extend({
+  
+  '-init' : function(options) { // this will be called before the herited init method
+    console.log('before init');
+  },
+
+  '+init' : function(options) { // this will be called after the herited init method
+    console.log('after init');
+  }
+
+});
+
+var instance = new MySubClass; // will log 'before init', 'init', 'after init' 
+```
+
+#### Publish/Subscribe
+```javascript
+var subscription = instance.on('foo', function() {
+  console.log('bar');
+});
+
+instance.fire('foo'); // will log 'bar'
+subscription.un(); // un stands for unsubscribe, I find unsubscribe too hard to type :D!
+instance.fire('foo'); // will do nothing
+```
+
+#### Views
+You will need to include [toDOM](https://github.com/KspR/toDOM) for this usage.
+```javascript
+var instance = new Seed.extend({
+  tpl : { // tpl stands for template, you can pass anything that toDOM can take!
+    tag : 'div Click me!',
+    events : {
+      click : function() {
+        instance.fire('click');
+      }
+    }
+  },
+
+  '+init' : function() {
+    document.body.appendChild(this.el);
+  }
+});
+
+instance.on('click', function() {
+  console.log('yay!');
+});
+
+
+### About
+
+####  Who uses sandjs ?
+* [Whibo](http://whibo.com) (collaborative real-time whiteboard)
+
+#### Authors 
+* [Pierre Colle](https://github.com/piercus)
+* [Sam Ton That](https://github.com/KspR)
---
+++ Seed.js
@@ -0,0 +1,213 @@
+(function(name) {
+
+  var extend = function(o, p) {
+    for (var i in p) {
+      o[i] = p[i];
+    }
+    return o;
+  };
+
+  var clone = function(o) {
+    var r = {};
+    for (var i in o) r[i] = o[i];
+    return r;
+  };
+
+  var Seed = window[name] = function() {
+    this.init.apply(this, arguments);
+  };
+
+  Seed.prototype = {
+    init : function(o) {
+      this.$subs = []; // sub-instances
+      this.$listeners = {}; // { eventName : [fn*] }
+      this.$observers = []; // [sub*]
+
+      if (this.isMediator) {
+        this.$responses = {};
+        if (this.respondsTo) {
+          for (var i in this.respondsTo) {
+            this.$responses[i] = this.respondsTo[i];
+          }
+        }
+
+        this.mediator = this;
+      }
+
+      if (o) {
+        if (o.mediator) this.mediator = o.mediator;
+        if (o.super) this.super = o.super;
+      }
+
+      this.$setOptions(o || {});
+      if (this.tpl) {
+        this.el = toDOM(typeof(this.tpl) === 'function' ? this.tpl() : this.tpl, this);
+        if (o && o.parentEl) o.parentEl.appendChild(this.el);
+      }
+    },
+
+    query : function(query) {
+      return this.mediator.$responses[query].apply(this.mediator, arguments);
+    },
+
+    destroy : function() {
+      this.detachObservers(); // detaches all events
+      for (var i = -1, n = this.$subs.length; ++i < n; ) this.$subs[i].destroy();
+      if (this.el && this.el.parentNode) this.el.parentNode.removeChild(this.el);
+
+      this.fire('destroy');
+    },
+
+    fire : function(event) {
+      var events = this.$listeners[event];
+      var a = Array.prototype.slice.call(arguments);
+      a.shift();
+
+      if (events) {
+        for (var i = events.length; i--; ) if (events[i]) events[i].apply(this, a); // last listener called first
+        return true; // at least one event has been fired
+      }
+      else {
+        return false; // no events fired
+      }
+    },
+
+    detachObservers : function() { // detaches all events that this instance has subscribed to
+      for (var i = this.$observers.length; i--; ) this.$observers[i].un();
+      this.$observers = [];
+    },
+
+    on : function(event, f, scope) {
+      var split = event.split(/ /g),
+          observers = [],
+          self = this;
+
+      for (var i = -1, n = split.length; ++i < n; ) {
+        var event = split[i];
+        if (!this.$listeners[event]) this.$listeners[event] = [];
+        this.$listeners[event].push(f);
+
+        var observer = {
+          un : function() {
+            self.$unbind(event, f);
+          }
+        };
+
+        observers.push(observer);
+        scope && scope.$observers.push(observer);
+      }
+
+      return {
+        un : function() {
+          for (var i = -1, n = observers.length; ++i < n; ) observers[i].un();
+        }
+      };
+    },
+
+    $unbind : function(event, f) {
+      for (var i = this.$listeners[event].length; i--; ) {
+        if (this.$listeners[event][i] === f) this.$listeners[event].splice(i, 1);
+      }
+    },
+    
+    $setOptions : function(o) {
+      if (typeof(__log) !== 'undefined') console.log('LOG', o);
+      if (this.options) {
+        if (typeof(this.options) === 'function') this.options = this.options();
+        for (var i in this.options) {
+          if (typeof(o[i]) !== 'undefined') this[i] = o[i];
+          else this[i] = this.options[i];
+        }
+      }
+    },
+
+    //=== retro
+    sub : function() {
+      return this.create.apply(this, arguments);
+    },
+    //---
+
+    create : function(C, o, as) {
+      if (!o) o = {};
+      o.mediator = this.isMediator ? this : this.mediator;
+      o.super = this;
+
+      C = new C(o);
+      this.$subs.push(C);
+      if (!C.isMediator && C.respondsTo) {
+        for (var i in C.respondsTo) {
+          C.mediator.$responses[i] = function() {
+            return C.respondsTo.apply(C, arguments);
+          };
+        }
+      }
+      if (as) this[as] = C;
+      return (C);
+    },
+
+    show : function() {
+      this.el.style.display = 'block';
+
+      this.fire('show');
+    },
+
+    hide : function() {
+      this.el.style.display = 'none';
+
+      this.fire('hide');
+    }
+
+  };
+
+  var pm = function(C, obj) {
+    for (var i in obj) {
+      if (i[0] === '+' || i[0] === '-') {
+        (function() {
+          var pmMethod = C.prototype[i], method = C.prototype[i.slice(1, i.length)], before, after;
+          if (i[0] === '+') {
+            before = method;
+            after = pmMethod;
+          }
+          else {
+            before = pmMethod;
+            after = method;
+          }
+
+          var res;
+          if (typeof(before) === 'function' && typeof(after) === 'function') {
+            C.prototype[i.slice(1, i.length)] = function() {
+              before.apply(this, arguments);
+              return after.apply(this, arguments);
+            };
+          }
+          else if (typeof(before) === 'function' || typeof(after) === 'function') {
+            //throw 'Should not happen';
+          }
+          else {
+            C.prototype[i.slice(1, i.length)] = extend(clone(before), after);
+          }
+        })();
+      }
+    }
+  };
+
+  Seed.augment = function(o) {
+    for (var i in o) this.prototype[i] = o[i];
+    pm(this, o);
+  };
+
+  Seed.extend = function(obj) {
+    var C = function(o) {
+      if (typeof(o) !== 'boolean' || o !== false) this.init.apply(this, arguments);
+    };
+
+    C.prototype = extend(new this(false), obj);
+    
+    pm(C, obj); // handling of +- methods
+
+    C.extend = obj.extend || this.extend;
+    C.augment = obj.augment || this.augment;
+    return C;
+  };
+
+})('Seed');
